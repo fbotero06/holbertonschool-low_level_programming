d164f83b84934218ca2f9ee1f25d57820c53dd86
fbotero06 <boteroso360@hotmail.com>
2021-09-30 16:26:22 -0500

    q

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#include <studio.h>
-/**
-Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
-*/
+#include <stdio.h>
+/* something */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
